4a522d6a2e337e085f52f96945a25c2ecedfc606
David Kapp <davekapp@gmail.com>
2012-02-15 13:34:19 -0700

    Initial Commit

---
+++ CoshxTechlunchTests/CoshxTechlunchTests.h
@@ -0,0 +1,13 @@
+//
+//  CoshxTechlunchTests.h
+//  CoshxTechlunchTests
+//
+//  Created by David Kapp on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoshxTechlunchTests : SenTestCase
+
+@end
---
+++ CoshxTechlunchTests/CoshxTechlunchTests.m
@@ -0,0 +1,32 @@
+//
+//  CoshxTechlunchTests.m
+//  CoshxTechlunchTests
+//
+//  Created by David Kapp on 2/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoshxTechlunchTests.h"
+
+@implementation CoshxTechlunchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoshxTechlunchTests");
+}
+
+@end
